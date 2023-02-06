20830fce1b34866712ffacafac775d0ad09c1678
G6gct <gct31@cam.ac.uk>
2023-02-06 23:55:53 +0000

    commit

--- Task 1E.py
+++
@@ -1,6 +0,0 @@
-from floodsystem.geo import rivers_by_station_number
-from floodsystem.stationdata import build_station_list
-
-stations = build_station_list()
-print(f"The top 9 rivers with the most stations: \n {rivers_by_station_number(stations,9)}")
-print(f"The top 20 rivers with the most stations: \n {rivers_by_station_number(stations,20)}")
