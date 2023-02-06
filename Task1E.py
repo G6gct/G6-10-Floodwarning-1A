3ff79186de165bd19e2bcdd955edef28992424dc
G6gct <gct31@cam.ac.uk>
2023-02-06 23:53:40 +0000

    William

--- Task1E.py
+++ Task1E.py
@@ -1,6 +1,6 @@
 from floodsystem.geo import rivers_by_station_number
 from floodsystem.stationdata import build_station_list
-from test_stationdata import test_build_station_list
+
 
 stations = build_station_list()
 print(f"The top 9 rivers with the most stations: \n {rivers_by_station_number(stations,9)}")
