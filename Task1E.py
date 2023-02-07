02530076d63714ea74ae949fc899a1c990b8a9f6
G6gct <gct31@cam.ac.uk>
2023-02-07 11:08:53 +0000

    Task 1E and 1F modified

--- Task1E.py
+++ Task1E.py
@@ -2,6 +2,13 @@ from floodsystem.geo import rivers_by_station_number
 from floodsystem.stationdata import build_station_list
 
 
-stations = build_station_list()
-print(f"The top 9 rivers with the most stations: \n {rivers_by_station_number(stations,9)}")
-print(f"The top 20 rivers with the most stations: \n {rivers_by_station_number(stations,20)}")
+
+def run():
+    stations = build_station_list()
+    print(f"The top 9 rivers with the most stations: \n {rivers_by_station_number(stations,9)}")
+    print(f"The top 20 rivers with the most stations: \n {rivers_by_station_number(stations,20)}")
+
+if __name__ == "__main__":
+    print("*** Task 1E: CUED Part IA Flood Warning System ***")
+    run()
+
