8f7132f3ee57d7e39b7f3bfcf741e62175bc0083
G6gct <gct31@cam.ac.uk>
2023-02-26 18:23:00 +0000

    William: Task 2C completed

--- Task2C.py
+++ Task2C.py
@@ -1,1 +1,13 @@
 #started
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    for station in stations_highest_rel_level(stations, 10):
+        print (station.name, station.relative_water_level())
+
+if __name__ == "__main__":
+    run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -7,3 +7,14 @@ def stations_level_over_threshold(stations, tol):
         if station.relative_water_level() != None and station.relative_water_level() > tol:
             output.append(tuple((station, station.relative_water_level())))
     return sorted_by_key(output, 1, True)
+
+def stations_highest_rel_level(stations, N):
+    output = []
+    for station in stations:
+        if station.relative_water_level() != None:
+            output.append(tuple((station, station.relative_water_level())))
+    output = sorted_by_key(output, 1, True)
+    output_final = [ ]
+    for n in output:
+        output_final.append(n[0])
+    return output_final[:N]
