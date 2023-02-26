c368d44e9c730224f51771eff7251a4dae7b07d7
G6gct <gct31@cam.ac.uk>
2023-02-26 17:52:16 +0000

    commit

--- Task2B.py
+++ Task2B.py
@@ -9,7 +9,7 @@ def run():
 
     stations_over_the_threshold = stations_level_over_threshold(stations, 0.8)
     for station in stations_over_the_threshold:
-        print (station[0], station[1])
+        print (station[0].name, station[1])
 
 if __name__ == "__main__":
     run()
