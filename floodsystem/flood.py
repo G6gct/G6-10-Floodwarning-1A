bb8eef6c59fec369c7b147dad6e264d70ffb09ba
G6gct <gct31@cam.ac.uk>
2023-02-26 17:50:17 +0000

    commit

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -4,6 +4,6 @@ from .stationdata import build_station_list
 def stations_level_over_threshold(stations, tol):
     output = []
     for station in stations:
-        if station.relative_water_level() > tol:
+        if station.relative_water_level() != None and station.relative_water_level() > tol:
             output.append(tuple((station, station.relative_water_level())))
     return sorted_by_key(output, 1, True)
