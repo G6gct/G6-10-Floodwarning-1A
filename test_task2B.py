7fead61cc8193815904a34933f1bd1e30995efa0
G6gct <gct31@cam.ac.uk>
2023-02-28 11:07:00 +0000

    Test 2B completed

---
+++ test_task2B.py
@@ -0,0 +1,16 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_level_over_threshold
+from floodsystem.station import MonitoringStation
+
+stations = build_station_list()
+
+def test_stations_level_over_threshold():
+    list = stations_level_over_threshold(stations, 0.6)
+    counter = 0
+    for station in stations:
+        if MonitoringStation.relative_water_level(station) != None and MonitoringStation.relative_water_level(station) > 0.6:
+            counter += 1
+        else:
+            None
+    print(counter, len(list))
+    assert counter == len(list)
